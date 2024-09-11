de23d16fc1d98ddcb0ae7971974635b7fa288470
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-09-11 13:32:34 -0700

    day 2 next js intro

--- src/app/(pages)/WelcomePage/page.tsx
+++ src/app/(pages)/WelcomePage/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+import { Button } from '@nextui-org/react'
 import React from 'react'
 
 const WelcomePage = () => {
   return (
     <>
-        <div>Welcome Page</div>
+      <h1 className='text-center text-3xl mt-3'>Welcome to the Blog</h1>
+      <p className='text-center mb-[20%]'>click to see anything and everything</p>
+      <div className='flex justify-center'>
+        <Button color={"primary"} variant='shadow' onClick={() => console.log("I was clicked")}>Button</Button>
+      </div>
     </>
   )
 }
